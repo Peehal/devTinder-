432e319c2ef8eed757e9a512baf3bd7d389788de
“Peehal” <sinhapeehal@gmail.com>
2025-08-10 16:29:30 +0530

    created express server

---
+++ src/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const app = express();
+
+// app.use((req, res) => {
+//     res.send("Hello from the server!!");
+    
+// })
+app.use("/hello", (req, res) => {
+    res.send("Hello Hello Hello !!");
+    
+})
+app.use( "/test", (req, res) => {
+    res.send("namaste from the Pihu - test!!");
+    
+})
+
+app.listen(3000, () =>{
+    console.log("Server is successfully reading");
+    
+});
+
+
+
+
