407495a508821db9211d113d2368cf9cc534fe49
Rhythmbits <sinhapeehal@gmail.com>
2025-08-13 00:58:40 +0530

    Update Database.js

--- src/config/Database.js
+++ src/config/Database.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const connectDB  = async () =>{
 
-    await mongoose.connect("mongodb+srv://<new_user>:<new_pass>@cluster0.ndfc21x.mongodb.net/devTinder ");
+    // await mongoose.connect("mongodb+srv://<new_user>:<new_pass>@cluster0.ndfc21x.mongodb.net/devTinder ");
 
     await mongoose.connect(process.env.MONGO_URI);
     
