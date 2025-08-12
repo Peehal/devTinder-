af67a948890ceca79931301725dd60ac5f30bbfa
“Peehal” <sinhapeehal@gmail.com>
2025-08-13 00:52:06 +0530

    Commit

--- src/config/Database.js
+++ src/config/Database.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const connectDB  = async () =>{
+
     await mongoose.connect("mongodb+srv://<new_user>:<new_pass>@cluster0.ndfc21x.mongodb.net/devTinder ");
+
+    await mongoose.connect(process.env.MONGO_URI);
+    
 };
 
 module.exports = connectDB;
