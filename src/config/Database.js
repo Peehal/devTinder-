7f3d06486ae63d0120f7ea03a737cf6db89ca153
Rhythmbits <sinhapeehal@gmail.com>
2025-08-13 00:30:06 +0530

    Update Database.js

--- src/config/Database.js
+++ src/config/Database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB  = async () =>{
-    await mongoose.connect("mongodb+srv://PeehuRoh:Rohan@cluster0.ndfc21x.mongodb.net/devTinder ");
+    await mongoose.connect("mongodb+srv://<new_user>:<new_pass>@cluster0.ndfc21x.mongodb.net/devTinder ");
 };
 
 module.exports = connectDB;
