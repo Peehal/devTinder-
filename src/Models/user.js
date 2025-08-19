058d14699b26b0aba95c59e397b24b76d01f4951
“Peehal” <sinhapeehal@gmail.com>
2025-08-19 16:28:51 +0530

    Exploring validator library

--- src/Models/user.js
+++ src/Models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
     firstName : {
@@ -17,34 +18,45 @@ const userSchema = new mongoose.Schema({
         required: true, 
         unique: true,
         lowercase:true,
-        trim:true
+        trim:true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("in valid email" + value);
+            }
+        }
     },
     password : {
         type : String, 
         required: true, 
-        validate(value) {
-            let errors = [];
+//         validate(value) {
+//             let errors = [];
 
-    if (!/[A-Z]/.test(value)) {
-      errors.push("Password must contain at least one uppercase letter");
-    }
+//     if (!/[A-Z]/.test(value)) {
+//       errors.push("Password must contain at least one uppercase letter");
+//     }
 
-    if (!/[0-9]/.test(value)) {
-      errors.push("Password must contain at least one number");
-    }
+//     if (!/[0-9]/.test(value)) {
+//       errors.push("Password must contain at least one number");
+//     }
 
-    if (!/[@$!%*?&]/.test(value)) {
-      errors.push("Password must contain at least one special character (@, $, !, %, *, ?, &)");
-    }
+//     if (!/[@$!%*?&]/.test(value)) {
+//       errors.push("Password must contain at least one special character (@, $, !, %, *, ?, &)");
+//     }
 
-    if (value.length < 8) {
-      errors.push("Password must be at least 8 characters long");
-    }
+//     if (value.length < 8) {
+//       errors.push("Password must be at least 8 characters long");
+//     }
 
-    if (errors.length > 0) {
-      throw new Error(errors.join(" | "));
-    }
-  }
+//     if (errors.length > 0) {
+//       throw new Error(errors.join(" | "));
+//     }
+//   }
+        validate(value){
+            if(!validator.isStrongPassword(value)){
+                throw new Error("Your password is not a strong password " + value);
+            }
+        }
+
 
 },
     age : {
@@ -64,7 +76,13 @@ const userSchema = new mongoose.Schema({
     },
     photoUrl :{
         type: String, 
-        default:""
+        default:"",
+        validate(value){
+            if(!validator.isURL(value)){
+                throw new Error("in valid url" + value);
+            }
+        }
+
     },
     about :{
         type:String,
