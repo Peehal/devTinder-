499919ecf7d9b5c2ff0174472dd363674a60bbec
“Peehal” <sinhapeehal@gmail.com>
2025-08-24 10:20:14 +0530

    Get/user/connection/from&to

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,6 +4,8 @@ const { userAuth } = require("../middlewares/auth");
 const userRouter = express.Router();
 const ConnectionRequest = require("../Models/connectionRequest")
 
+const USER_SAVE_DATA ="firstName lastName "
+
 userRouter.get("/user/request/received", userAuth, async(req, res) =>{
 
     try {
@@ -12,7 +14,7 @@ userRouter.get("/user/request/received", userAuth, async(req, res) =>{
         const connectionRequest = await ConnectionRequest.find({
             toUserId:loggedInUser._id,
             status:"interested",
-        }).populate("fromUserId","firstName lastName ")
+        }).populate("fromUserId","firstName lastName " )
         // }).populate("fromUserId",["firstName", "lastName"])
 
         res.json({
@@ -26,5 +28,28 @@ userRouter.get("/user/request/received", userAuth, async(req, res) =>{
 
 })
 
+
+userRouter.get("/user/connections", userAuth, async(req, res) => {
+    try {
+        const loggedInUser = req.user;
+
+        const connectionRequest = await ConnectionRequest.find({
+            $or:[
+                {fromUserId:loggedInUser._id, status :"accepted"}, 
+                {toUserId:loggedInUser, status:"accepted"}
+            ]
+        }).populate("fromUserId", "firstName lastName ").populate("toUSerId", "firstName lastName")
+
+        const data = connectionRequest.map((row) => row.fromUserId); 
+
+        res.json({
+            data 
+        })
+    } catch (error) {
+        res.status(400).send("ERROR:" + error.message)
+    }
+
+
+})
 
 module.exports = userRouter;
